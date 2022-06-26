47078fb0be111fcc63d54cfc8a9c137afae3dd68
David Ackerman <david@dvacreative.com>
2022-06-26 12:33:38 -0400

    ok

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// (A) DATABASE SETTINGS
-define("DB_HOST", "localhost");
-define("DB_NAME", "php_rest_api_user_auth_demo");
-define("DB_CHARSET", "utf8");
-define("DB_USER", "dva_desktop");
-define("DB_PASSWORD", "V!triol@2027");

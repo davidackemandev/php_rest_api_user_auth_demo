c983fe8226652e96e87647b49adc6f305849ff52
David Ackerman <david@dvacreative.com>
2022-06-26 12:29:49 -0400

    prog

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 // (A) DATABASE SETTINGS
 define("DB_HOST", "localhost");
-define("DB_NAME", "notevdij_srv2");
+define("DB_NAME", "php_rest_api_user_auth_demo");
 define("DB_CHARSET", "utf8");
-define("DB_USER", "notevdij_srv2");
+define("DB_USER", "dva_desktop");
 define("DB_PASSWORD", "V!triol@2027");
  
 // (B) JWT STUFF
--- createtables.sql
+++ createtables.sql
@@ -1,17 +1,18 @@
+CREATE SCHEMA `php_rest_api_user_auth_demo` ;
+
 CREATE TABLE `accounts` (
  `account_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
  `account_email` varchar(255) CHARACTER SET utf8 NOT NULL,
  `account_passwd` varchar(255) CHARACTER SET utf8 NOT NULL,
  `account_reg_time` timestamp NOT NULL DEFAULT current_timestamp(),
  `account_enabled` tinyint(1) unsigned NOT NULL,
- `account_notes` longtext CHARACTER SET utf8 NOT NULL,
  PRIMARY KEY (`account_id`),
  UNIQUE KEY `account_email` (`account_email`)
-) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=latin1
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `account_sessions` (
  `session_id` varchar(255) CHARACTER SET utf8 NOT NULL,
  `account_id` int(10) unsigned NOT NULL,
  `login_time` timestamp NOT NULL DEFAULT current_timestamp(),
  PRIMARY KEY (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
